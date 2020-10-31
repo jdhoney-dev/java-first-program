b396ff66cdf4fa7de455d4a280466b64b5857f6e
jeremy.honeycutt <jeremy.honeycutt@openedgepay.com>
2020-10-30 22:15:48 -0600

    Completed module

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,36 @@
+package com.h2;
+
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+    private static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         System.out.println("enter your name");
+         String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+
+        float bestRate = getRates(loanTermInYears);
+
+        if(bestRate == 0.0f){
+            System.out.println("No available rates for term" + loanTermInYears + " years");
+        }else{
+            System.out.println("Best Avvailable Rate: " + getRates(loanTermInYears) + "%");
+            scanner.close();
+        }
+
+
+    }
+
+    public static float getRates(int loanTermInYears){
+        float result = 0.0f;
+        if(bestRates.containsKey(loanTermInYears)){
+            result = bestRates.get(loanTermInYears);
+        }
+        return result;
+    }
+}
